8513b52fee7782b9b65fa59748f2498bd9bc8792
Benjamin <Benjamin@LAPTOP-V32R4J24>
2018-10-19 12:28:22 -0600

    My First Cucumber checkin

---
+++ CucumberAuto/src/test/java/com/basic/backgroundSD/RunBackgroudTest.java
@@ -0,0 +1,35 @@
+package com.basic.backgroundSD;
+
+import org.junit.runner.RunWith;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+	@RunWith(Cucumber.class)
+	@CucumberOptions(
+			
+			monochrome=true,
+			//dryRun =true,
+			
+			features= {"src/test/resources/com/basic/backgroudFF/"},
+			
+			glue= {"com/basic/backgroundSD/"},
+			
+			plugin= {"pretty",
+					 "html:target/cucumber-htmlreport", 
+					 "json:target/cucumber-report1.json",
+					 "com.cucumber.listener.ExtentCucumberFormatter:target/ExtentReport1.html"
+					
+
+			}
+			
+			)
+	
+	public class RunBackgroudTest {
+	
+		
+	}
+	
+	
+	
+
---
+++ CucumberAuto/src/test/resources/com/basic/datatableFF/DataTableFF.feature
@@ -0,0 +1,11 @@
+Feature: Create acount of Facebook3
+  As a user u need to open facebook home page and do the validations
+
+
+  Scenario: Validate create user multiple fields31
+    Given User need to be on Facebook login page
+    When Enter followind data
+    	|userName |userSurname |Mobile|
+    	|Tom      |Hanks       |1234  |
+    	|Jack     |Danial      |5678  |
+    	
